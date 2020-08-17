cdd99740bf4af5012206b398b0867de34ff37ba0
vlad-74 <kurguzov74@gmail.com>
2020-08-17 11:16:34 +0300

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+
+import { fromEvent, interval, Subject } from 'rxjs';
+import { switchMap, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comp',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   bool = false;
+  show = true;
+  private readonly destroyed$ = new Subject();
 
-   ngOnInit() {
-    setTimeout(()=>{this.bool = true})
+  ngOnInit() {
+    fromEvent(document, 'click')
+      .pipe(
+        takeUntil(this.destroyed$),
+        switchMap((ev) => interval(1000))
+      )
+      .subscribe(x => {
+        this.bool = true
+        this.show = x < 60;
+        console.log(x)
+      });
+    document.body.click()
   }
 
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 }
